0a566841b9e13a64e7a34c3f16cc80f189a83f2f
Justin Hammond <justin.hammond@rackspace.com>
2012-10-01 13:36:28 -0500

    Updated req. networks for XML

--- wafflehaus/requestnetworks.py
+++ wafflehaus/requestnetworks.py
@@ -26,6 +26,23 @@ class RequestNetworks(base_wsgi.Middleware):
         self.public_net = local_config.get("public_net", DEFAULT_PUBLIC_NET)
         self.service_net = local_config.get("service_net", DEFAULT_SERVICE_NET)
 
+    def get_servers_from_json(self, req):
+        body = jsonutils.loads(req.body)
+        networks = body["server"]["networks"]
+        servers = []
+        for net in networks:
+            servers.append(net["uuid"])
+        return servers
+
+    def get_servers_from_xml(self, req):
+        server_dict = req.body["server"]
+        servers = []
+        if "networks" in server_dict:
+            networks = server_dict.get("networks")
+            for network in networks:
+                servers.append(network["uuid"])
+        return servers
+
     @webob.dec.wsgify(RequestClass=wsgi.Request)
     def __call__(self, req, **local_config):
         verb = req.method
@@ -44,20 +61,22 @@ class RequestNetworks(base_wsgi.Middleware):
         pathparts = path.split("/")
         pathparts.insert(0, verb)
         pathparts = set(pathparts)
-        bootcheck = set(["POST", projectid, "servers"]) 
+        bootcheck = set(["POST", projectid, "servers"])
 
         if len(pathparts.intersection(bootcheck)) == len(bootcheck):
             if not req.body or len(req.body) == 0:
                 raise exc.HTTPUnprocessableEntity("Body is missing")
-            body = jsonutils.loads(req.body)
-            networks = body["server"]["networks"]
+            if req.content_type and "xml" in req.content_type:
+                servers = self.get_servers_from_xml(req)
+            else:
+                servers = self.get_servers_from_json(req)
             has_service = False
-            has_public = False
-            for net in networks:
-                if net["uuid"] == self.service_net:
+#            has_public = False
+            for server in servers:
+                if server == self.service_net:
                     has_service = True
-                elif net["uuid"] == self.public_net:
-                    has_public = True
+#                elif server == self.public_net:
+#                    has_public = True
             msg = "Service net required but missing"
             if not has_service:
                 raise exc.HTTPUnprocessableEntity(msg)
