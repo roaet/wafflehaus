7fdc2c8ca168084221a4b9823e0dd63d589d3521
Justin Hammond <justin.hammond@rackspace.com>
2014-02-24 11:32:24 -0600

    Made resource filter utility fx

--- wafflehaus/resource_filter/__init__.py
+++ wafflehaus/resource_filter/__init__.py
@@ -12,3 +12,36 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+from routes import Mapper
+
+__all__ = ['_parse_resources']
+
+
+def parse_resources(resources):
+    result = {}
+    if not resources:
+        return
+    work_list = [s.strip() for s in resources.split(',')]
+    for res in work_list:
+        res_split = res.split()
+        methods = [m.upper() for m in res_split[:-1]]
+        resource = res_split[-1]
+        if resource not in result:
+            result[resource] = []
+        for m in methods:
+            result[resource].append(m)
+    return result
+
+
+def matched_request(request, resource_list):
+    if len(resource_list) == 0:
+        return False
+    map = Mapper()
+    for resource, data in resource_list.iteritems():
+        map.connect(None, resource, controller=','.join(data))
+    res = map.routematch(request.path)
+    if res is None:
+        return False
+    if request.method in res[0]['controller'].split(','):
+        return True
+    return False
